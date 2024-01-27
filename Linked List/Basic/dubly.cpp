ca92e1e81a0a3f04dd52dbeeae0a1cc1f1cb1d97
Badhon Nath Joy <joy184110@gmail.com>
2024-01-27 20:42:16 +0600

    Dubly insertion added

--- Linked List/Basic/dubly.cpp
+++ Linked List/Basic/dubly.cpp
@@ -22,6 +22,45 @@ void insertAtHead(Node* &head, int d){
     head = temp;
 }
 
+void insertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    temp -> prev = tail;
+    tail = temp;
+
+}
+
+void InsertAtPosition(Node* &head, Node* &tail, int position, int d)
+{
+    //if insert at first position
+    if(position == 1)
+    {
+        insertAtHead(head,d);
+        return;   
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+    while (cnt < position-1){
+        temp= temp -> next;
+        cnt++;
+    }
+    // inserting at last position
+    if(temp -> next == NULL)
+    {
+        insertAtTail(tail,d);
+        return;
+        
+    }
+    // creating a node for d
+    Node* nodeToInsert = new Node(d);
+    nodeToInsert -> next = temp -> next;
+    temp -> next -> prev = nodeToInsert;
+    temp -> next = nodeToInsert;
+    nodeToInsert -> prev = temp;
+}
+
+
 
 void print(Node* head){
     Node* temp= head;
@@ -51,9 +90,14 @@ int main()
 {
     Node* node1 = new Node(10);
     Node* head = node1;
+    Node* tail = node1;
+
     print(head);
     cout<<"Length: "<< getLength(head)<<endl;
 
     insertAtHead(head, 11);
+    insertAtTail(tail, 25);
+    InsertAtPosition(head, tail, 2, 100);
+    
     print(head);
 }
